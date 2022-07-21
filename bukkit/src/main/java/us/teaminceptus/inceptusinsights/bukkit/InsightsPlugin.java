48efd1e6d6a410a439f7ba8057d56049e8820e68
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-20 22:37:28 -0500

    Base Bukkit Plugin

--- bukkit/src/main/java/us/teaminceptus/inceptusinsights/bukkit/InsightsPlugin.java
+++ bukkit/src/main/java/us/teaminceptus/inceptusinsights/bukkit/InsightsPlugin.java
@@ -1,9 +1,32 @@
 package us.teaminceptus.inceptusinsights.bukkit;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import us.teaminceptus.inceptusinsights.InsightsServer;
+
+import java.net.Socket;
 
 public final class InsightsPlugin extends JavaPlugin {
 
+    private boolean portInUse() {
+        try {
+            new Socket("localhost", getConfig().getInt("port", 30325)).close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        if (portInUse()) {
+            getLogger().severe("Port " + getConfig().getInt("port", 30325) + " is already in use. Please choose a different port.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        InsightsServer.setLogger(getLogger());
+    }
 
 }
