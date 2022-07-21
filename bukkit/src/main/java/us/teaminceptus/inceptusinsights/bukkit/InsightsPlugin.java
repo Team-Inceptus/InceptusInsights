0c3e343869e023e4953b7d210f86820cae551606
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-21 14:24:29 -0500

    Update InsightsPlugin.java
    - Add Port Argument
    - Stop running server on disable

--- bukkit/src/main/java/us/teaminceptus/inceptusinsights/bukkit/InsightsPlugin.java
+++ bukkit/src/main/java/us/teaminceptus/inceptusinsights/bukkit/InsightsPlugin.java
@@ -3,6 +3,7 @@ package us.teaminceptus.inceptusinsights.bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import us.teaminceptus.inceptusinsights.InsightsServer;
 
+import java.io.File;
 import java.net.Socket;
 
 public final class InsightsPlugin extends JavaPlugin {
@@ -19,14 +20,24 @@ public final class InsightsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();
-
         if (portInUse()) {
             getLogger().severe("Port " + getConfig().getInt("port", 30325) + " is already in use. Please choose a different port.");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
 
+        InsightsServer.setInformationFetchers(new BukkitServerInformation(this), new BukkitPlayerInformation(this));
         InsightsServer.setLogger(getLogger());
+        InsightsServer.start("--port=" + getConfig().getInt("port", 30325));
+        getLogger().info("Loaded InceptusInsights - Bukkit Version");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling Server...");
+        if (InsightsServer.isRunning())
+            InsightsServer.stop();
+        else getLogger().info("Server is not running.");
     }
 
 }
